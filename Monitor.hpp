b1f406b3274e3ca3232af485c680f05004c0ee45
shkiper325 <shkiper325@gmail.com>
2017-02-03 03:03:45 +0300

    Added empty monitor

---
+++ Monitor.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Blessings_ns {
+  template <class InputSymbol>
+  class ReadStream;
+
+  template <class OutputSymbol, class Color>
+  class WriteStream;
+
+  template <class InputSymbol, class OutputSymbol, class Color>
+  class Monitor {
+  };
+}
