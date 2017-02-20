8355250aea7ec94dc93dd3f86662a102c2568716
Destabilizer <destabilizer@opmbx.org>
2017-02-21 02:32:50 +0300

    Completed Monitor::Iterator and some methods of Monitor

--- Monitor.cpp
+++ Monitor.cpp
@@ -6,67 +6,143 @@
 
 namespace Blessings {
 
-  Monitor::Monitor(TerminalIO Term, int MaxSize) {
-    if (MaxSize <= 0) throw Monitor::Error("wrong MaxSize");
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Monitor(TerminalIO Term, int MaxSize) {
+    if (MaxSize <= 0) throw Monitor::Error();   // ::Error("wrong MaxSize")
     termIO = Term;
-    grid = new MonitorCell[MaxSize];
     maxSize = MaxSize;
-    stopPosition = MaxSize;
     res.width=1; res.height=0;
-    pointer=0;
+    grid = new MonitorCell[maxSize];
   }
 
-  Monitor::Monitor(const Monitor& monitor) {
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Monitor(const Monitor& monitor) {
     termIO = monitor.termIO;
     maxSize = monitor.maxSize;
-    stopPosition = monitor.stopPosition;
     res = monitor.res;
-    pointer = monitor.pointer;
     delete grid;
     grid = new MonitorCell[maxSize];
-    for (i=0;i++;i<stopPosition) {
+    for (i=0;i++;i<maxSize)
       grid[i] = monitor.grid[i];
-    }
   }
 
-  Monitor& Monitor::operator=(const Monitor& monitor) {
+  template <class InS, class OutS>
+  Monitor& Monitor<InS,OutS>::operator=(const Monitor& monitor) {
     termIO = monitor.termIO;
     maxSize = monitor.maxSize;
-    stopPosition = monitor.stopPosition;
     res = monitor.res;
-    pointer = monitor.pointer;
     delete grid;
     grid = new MonitorCell[maxSize];
-    for (i=0;i++;i<stopPosition) {
+    for (i=0;i++;i<maxSize)
       grid[i] = monitor.grid[i];
-    }
   }
 
-  Monitor::~Monitor() {
-    delete grid;
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::~Monitor() {
+    delete [] grid;
   }
 
+  template <class InS, class OutS>
+  GridPos Monitor<InS,OutS>::positionOf(int index) const {
+    GridPos gp;
+    gp.x = index%res.width;
+    gp.y = index/res.height;
+    return gp;
+  }
 
-  MonitorCell& Monitor::operator[] (int p) {
-    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error("p out of range");
+  template <class InS, class OutS>
+  int Monitor<InS,OutS>::indexOf(GridPos gp) const {
+    return gp.x+gp.y*res.width;
+  }
+
+  template <class InS, class OutS>
+  MonitorCell& Monitor<InS,OutS>::operator[] (int p) {
+    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error();
+    //Monitor::Error("p out of range");
     return grid[p];
   }
 
-  MonitorCell Monitor::operator[] (int p) const {
-    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error("p out of range");
+  template <class InS, class OutS>
+  MonitorCell Monitor<InS,OutS>::operator[] (int p) const {
+    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error();
+    //Monitor::Error("p out of range");
     return grid[p];
   }
 
-  MonitorCell& Monitor::operator() (int x, int y) {
+  template <class InS, class OutS>
+  MonitorCell& Monitor<InS,OutS>::operator() (int x, int y) {
     int p = x+y*res.width;
-    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error("p out of range");
+    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error();
+    //Monitor::Error("p out of range");
     return grid[p];
   }
 
-  MonitorCell Monitor::operator() (int x, int y) const {
+  template <class InS, class OutS>
+  MonitorCell Monitor<InS,OutS>::operator() (int x, int y) const {
     int p = x+y*res.width;
-    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error("p out of range");
+    if ((p <= 0) || (p >= maxSize)) throw Monitor::Error();
+    //Monitor::Error("p out of range");
     return grid[p];
   }
 
+
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Iterator Monitor<InS,OutS>::Iterator(int pnt, int bnd) {
+    if ((pnt<0) || (pnt>bnd)) throw Monitor::Iterator::Error();
+    //Monitor::Iterator::Error("pointer out of range");
+    pointer = pnt;
+    stopPos = bnd;
+  }
+
+  template <class InS, class OutS>
+  MonitorCell& Monitor<InS,OutS>::Iterator::operator*() {
+    if (pointer==stopPos) throw Monitor::Iterator::EndError();
+    //Monitor::Iterator::EndError("pointer is at end");
+    return grid[pointer];
+  }
+
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Iterator& Monitor<InS,OutS>::Iterator::operator++() {
+    if (pointer==stopPos) throw Monitor::Iterator::EndError();
+    //Monitor::Iterator::EndError("pointer is at end");
+    pointer++;
+    return (*this);
+  }
+
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Iterator& Monitor<InS,OutS>::Iterator::operator++(int a) {
+    if (pointer==stopPos) throw Monitor::Iterator::EndError();
+    //Monitor::Iterator::EndError("pointer is at end");
+    Monitor::Iterator i = (*this);
+    ++(*this);
+    return i;
+  }
+
+  template <class InS, class OutS>
+  int Monitor<InS,OutS>::Iterator::currentIndex() {
+    return pointer;
+  }
+
+  template <class InS, class OutS>
+  GridPos Monitor<InS,OutS>::Iterator::currentPos() {
+    return positionOf(pointer);
+  }
+
+
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Iterator Monitor<InS,OutS>::begin() {
+    Monitor::Iterator i(0, res.width*res.height);
+    return i;
+  }
+
+  template <class InS, class OutS>
+  Monitor<InS,OutS>::Iterator Monitor<InS,OutS>::end() {
+    Monitor::Iterator i(res.width*res.height, res.width*res.height);
+    return i;
+  }
+}
+
+
+int main {
+  return 0;
 }
