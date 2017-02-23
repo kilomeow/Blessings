21f0868c8a9b6e8384881bc00de039ed093796f0
shkiper325 <shkiper325@gmail.com>
2017-02-23 07:31:30 +0300

    Added level editor to boxman

--- main.cpp
+++ main.cpp
@@ -40,6 +40,8 @@ struct BoxmanLevel {
 
   class Error {};
   class InitError : public Error {};
+  class ReadError : public Error {};
+  class WriteError : public Error {};
 
   BoxmanLevel(string s) {
     try {
@@ -70,8 +72,11 @@ struct BoxmanLevel {
         goals.push_back(c);
       }
     }
+    catch(InitError) {
+      throw;
+    }
     catch(...) {
-      throw InitError();
+      throw ReadError();
     }
 
     if(boxes.size()!=goals.size() || boxes.size()==0) throw InitError();
@@ -106,6 +111,151 @@ struct BoxmanLevel {
       for(auto itG=goals.begin(); itG!=goals.end(); ++itG) {
         if(itG->x>=xSize || itG->y>=ySize) throw InitError();
       }
+  }
+
+  void save(string s) {
+    try {
+      std::ofstream fs(s);
+
+      fs << xSize << ' ' << ySize << endl << endl;
+
+      fs << startPos.x << ' ' << startPos.y << endl << endl;
+
+      fs << boxes.size() << endl << endl;
+
+      for(int i=0; i<boxes.size(); ++i) {
+        fs << boxes[i].x << ' ' << boxes[i].y << endl;
+      }
+
+      fs << endl;
+
+      for(int i=0; i<goals.size(); ++i) {
+        fs << goals[i].x << ' ' << goals[i].y << endl;
+      }
+    }
+    catch(...) {
+      throw WriteError();
+    }
+  }
+};
+
+class EditorRoom {
+  BoxmanLevel level;
+
+  Coord pos;
+public:
+  class Error {};
+  class InitError : public Error {};
+  class BadLevel : public Error {};
+
+  EditorRoom(int xSize, int ySize) : level(vector<Coord>(1,Coord(1,1)), vector<Coord>(1,Coord(1,2)), xSize, ySize, Coord(0,0)){
+    if(level.xSize<=0 || level.ySize<=0) throw InitError();
+  }
+
+  void moveLeft() {
+    if(pos.x==0) return;
+    else --pos.x;
+  }
+
+  void moveRight() {
+    if(pos.x==level.xSize-1) return;
+    else ++pos.x;
+  }
+
+  void moveUp() {
+    if(pos.y==0) return;
+    else --pos.y;
+  }
+
+  void moveDown() {
+    if(pos.y==level.ySize-1) return;
+    else ++pos.y;
+  }
+
+  void print(TL* term) {
+    term->clearScreen();
+    term->hideCursor();
+
+    for(int x=0; x<=level.xSize; ++x) {
+      term->moveCursorTo(x+1, level.ySize+1);
+      term->print(SymbolUTF8("*"));
+    }
+    for(int y=0; y<=level.ySize; ++y) {
+      term->moveCursorTo(level.xSize+1, y+1);
+      term->print(SymbolUTF8("*"));
+    }
+
+    for(auto itG=level.goals.begin(); itG!=level.goals.end(); ++itG) {
+      term->moveCursorTo(itG->x+1, itG->y+1);
+      term->print(SymbolUTF8("!"));
+    }
+
+    term->moveCursorTo(level.startPos.x+1, level.startPos.y+1);
+    term->print(SymbolUTF8("$"));
+
+    for(auto itB=level.boxes.begin(); itB!=level.boxes.end(); ++itB) {
+      term->moveCursorTo(itB->x+1, itB->y+1);
+      term->print(SymbolUTF8("#"));
+    }
+
+    term->moveCursorTo(pos.x+1, pos.y+1);
+    term->print(SymbolUTF8("@"));
+  }
+
+  void editBox() {
+    bool exists=false;
+    auto it=level.boxes.begin();
+    for(; it!=level.boxes.end(); ++it) {
+      if(*it==pos) {
+        exists=true;
+        break;
+      }
+    }
+
+    if(exists) {
+      level.boxes.erase(it);
+    }
+    else {
+      level.boxes.push_back(pos);
+    }
+  }
+
+  void editGoal() {
+    bool exists=false;
+    auto it=level.goals.begin();
+    for(; it!=level.goals.end(); ++it) {
+      if(*it==pos) {
+        exists=true;
+        break;
+      }
+    }
+
+    if(exists) {
+      level.goals.erase(it);
+    }
+    else {
+      level.goals.push_back(pos);
+    }
+  }
+
+  void setStartPos() {
+    level.startPos=pos;
+  }
+
+  bool checkLevel() {
+    if(level.boxes.size()!=level.goals.size() || level.boxes.size()==0) return false;
+
+    for(auto itB=level.boxes.begin(); itB!=level.boxes.end(); ++itB) {
+      if(itB->x==level.startPos.x && itB->x==level.startPos.y) return false;
+    }
+
+    return true;
+  }
+
+  BoxmanLevel getLevel() {
+    if(!checkLevel()) throw BadLevel();
+
+    return level;
   }
 };
 
@@ -336,6 +486,214 @@ public:
     if(levelsFolder[levelsFolder.size()-1]!='/') levelsFolder+='/';
   }
 
+  void launchEditor(TL* term) {
+    if(!term->isReady()) throw TerminalIsNotReadyError();
+
+    int xSize=1;
+    int ySize=1;
+
+    term->clearScreen();
+    term->moveCursorTo(1,1);
+
+    cout << "Choose x size:" << endl;
+
+    term->moveCursorTo(1,2);
+    term->print(SymbolUTF8("1"));
+
+    for(;;) {
+      auto s=term->getSymbol();
+
+      if(s.size()==1) {
+        if(s[0].getString()=="a") {
+          if(xSize==1) continue;
+          else {
+            string s=to_string(xSize);
+            for(int i=1; i<=s.size(); ++i) {
+              term->moveCursorTo(i,2);
+              term->print(SymbolUTF8(" "));
+            }
+
+            --xSize;
+            s=to_string(xSize);
+            for(int i=1; i<=s.size(); ++i) {
+              term->moveCursorTo(i,2);
+              term->print(SymbolUTF8(string(1, s[i-1])));
+            }
+          }
+        }
+        else if(s[0].getString()=="d") {
+          ++xSize;
+          string s=to_string(xSize);
+          for(int i=1; i<=s.size(); ++i) {
+            term->moveCursorTo(i,2);
+            term->print(SymbolUTF8(string(1, s[i-1])));
+          }
+        }
+        else if(s[0].getString()=="\n") break;
+      }
+
+      usleep(10000);
+    }
+
+    term->clearScreen();
+    term->moveCursorTo(1,1);
+
+    cout << "Choose y size:" << endl;
+
+
+    term->moveCursorTo(1,2);
+    term->print(SymbolUTF8("1"));
+
+    for(;;) {
+      auto s=term->getSymbol();
+
+      if(s.size()==1) {
+        if(s[0].getString()=="a") {
+          if(ySize==1) continue;
+          else {
+            string s=to_string(ySize);
+            for(int i=1; i<=s.size(); ++i) {
+              term->moveCursorTo(i,2);
+              term->print(SymbolUTF8(" "));
+            }
+
+            --ySize;
+            s=to_string(ySize);
+            for(int i=1; i<=s.size(); ++i) {
+              term->moveCursorTo(i,2);
+              term->print(SymbolUTF8(string(1, s[i-1])));
+            }
+          }
+        }
+        else if(s[0].getString()=="d") {
+          ++ySize;
+          string s=to_string(ySize);
+          for(int i=1; i<=s.size(); ++i) {
+            term->moveCursorTo(i,2);
+            term->print(SymbolUTF8(string(1, s[i-1])));
+          }
+        }
+        else if(s[0].getString()=="\n") break;
+      }
+
+      usleep(10000);
+    }
+
+    EditorRoom room(xSize, ySize);
+    room.print(term);
+
+    term->moveCursorTo(1, ySize+3);
+    cout << "b to set box" << endl;
+    cout << "g to set goal" << endl;
+    cout << "h to set start pos" << endl;
+    cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+    cout << "q to quit main menu" << endl;
+
+    for(;;) {
+      auto s=term->getSymbol();
+
+      if(s.size()==1) {
+        if(s[0].getString()=="w") {
+          room.moveUp();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="a") {
+          room.moveLeft();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="s") {
+          room.moveDown();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="d") {
+          room.moveRight();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="b") {
+          room.editBox();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="g") {
+          room.editGoal();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="g") {
+          room.editGoal();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="h") {
+          room.setStartPos();
+          room.print(term);
+          term->moveCursorTo(1, ySize+3);
+          cout << "b to set box" << endl;
+          cout << "g to set goal" << endl;
+          cout << "h to set start pos" << endl;
+          cout << "p to save; save file is \"1\". Move it to levels folder to play" << endl;
+          cout << "q to quit main menu" << endl;
+        }
+        else if(s[0].getString()=="p") {
+          if(room.checkLevel()) {
+            BoxmanLevel level=room.getLevel();
+            level.save("1");
+            term->moveCursorTo(1, ySize+10);
+            cout << "Saved" << endl;
+          }
+          else {
+            term->moveCursorTo(1, ySize+10);
+            cout << "Bad level, change it to save" << endl;
+          }
+        }
+        else if(s[0].getString()=="q") {
+          break;
+        }
+      }
+
+      usleep(10000);
+    }
+  }
+
   void loadLevels() {
     levels.clear();
 
@@ -360,14 +718,6 @@ public:
 
   void launchLevel(TL* term, BoxmanLevel level) {
     if(!term->isReady()) throw TerminalIsNotReadyError();
-
-    int xSize=20;
-    int ySize=15;
-
-    vector<Coord> boxes={Coord(1,2), Coord(5,6)};
-    vector<Coord> goals={Coord(3,4), Coord(8,9)};
-
-    Coord startPos(9,10);
 
     Room room(level);
     room.print(term);
@@ -401,7 +751,7 @@ public:
       usleep(10000);
     }
 
-    term->moveCursorTo(1, ySize+3);
+    term->moveCursorTo(1, level.ySize+3);
     term->showCursor();
 
     string complete="Complete!\n";
@@ -411,13 +761,15 @@ public:
   }
 
   void launchMainMenu(TL* term) {
+    if(!term->isReady()) throw TerminalIsNotReadyError();
+
     term->hideCursor();
 
     for(;;) {
       term->clearScreen();
       term->moveCursorTo(1,1);
 
-      cout << "l to load levels list, q to quit" << endl;
+      cout << "l to load levels list, e to launch editor, q to quit" << endl;
       if(levels.size()==0) cout << "There is no levels loaded now" << endl;
       else cout << "AD to choose level, ENTER to start" << endl;
 
@@ -432,6 +784,10 @@ public:
             }
             else if(s[0].getString()=="q") {
               return;
+            }
+            else if(s[0].getString()=="e") {
+              launchEditor(term);
+              break;
             }
           }
 
@@ -485,6 +841,10 @@ public:
             }
             else if(s[0].getString()=="q") {
               return;
+            }
+            else if(s[0].getString()=="e") {
+              launchEditor(term);
+              break;
             }
           }
 
