988ba975ee4c8bcec0d8efd3a8d7ef95884cfaa6
Destabilizer <destabilizer@opmbx.org>
2017-02-27 07:05:11 +0300

    Test added

--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+//#include <chrono>
+//#include <thread>
 
 #include "Symbol/SymbolUTF8.hpp"
 #include "Symbol/Symbol.hpp"
@@ -10,20 +12,43 @@
 using namespace std;
 using namespace blessings;
 
-typedef TerminalIOANSILinux<SymbolUTF8, SymbolUTF8, PropertyANSI> TL;
-typedef Monitor<SymbolUTF8, SymbolUTF8> M;
+typedef SymbolUTF8 S;
+typedef PropertyANSI P;
+typedef TerminalIOANSILinux<S, S, P> TL;
+typedef Monitor<S, S> M;
+typedef MonitorCell<S> Cell;
 
 int main() {
-  TL * term;
+  TL term;
 
-  term->Init();
+  term.Init();
 
-  term->setNonCanonicalMode();
-  term->setEchoInhibition();
+  term.setNonCanonicalMode();
+  term.setEchoInhibition();
 
-  M monitor(term, 256);
+  M monitor(&term, 2000);
+  monitor.setResolution(50, 10);
+  //monitor.clearScreen();
+  
+  int t=0;
+  
+  for (int k=0;k++;k<5) {
+		cout << k << endl;
+		t++;
+	}
+  
+  cout << t << endl;
 
-  monitor.setResolution(10, 10);
-
+  //for (int k=0;k++;k<4) {
+	//	for (int s=0;s++;s<500) {
+	//		c = fscanf(in, "%c");
+	//		if (c != *"\n") {
+	//			monitor[s] = Cell(S(c), &P::defaultProperty);
+	//		}
+	//	}
+	//	monitor.draw(M::resChange::ignore);
+	//	this_thread::sleep_for(chrono::milliseconds(500));
+	//}
+	
   return 0;
 }
