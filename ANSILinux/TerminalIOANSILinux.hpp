6a7e35f29fa90ba8b38fa8a514f8293cc527608a
destabilizer <destabilizer@opmbx.org>
2017-02-28 16:08:47 +0300

    Filenames updated

--- ANSILinux/TerminalIOANSILinux.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace blessings {
-  template <class InS, class OutS, class PropertyT>
-  class TerminalIOANSILinux;
-}
-
-#include "TIOAL_PropANSI.hpp"
--- ANSILinux/WriteStreamLinux.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace blessings {
-  template <class OutS>
-  class WriteStreamLinux;
-}
-
-#include "WSL_SymbolUTF8.hpp"
